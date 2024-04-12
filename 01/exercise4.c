059d80388202a0e40294c5439fbc8bac03a097ac
1f10220137 <s1f102201379@iniad.org>
2024-04-12 12:41:02 +0900

    miss

--- 01/exercise4.c
+++ 01/exercise4.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int addnum(void){
+    int line_number = 1;
+    char buffer[1024];
+
+    while (gets(buffer, sizeof(buffer), stdin)){
+        if (feof(buffer)){
+            break;
+        }
+        char text = line_number + buffer;
+        puts(text);
+        line_number++;
+    }
+    return 0;
+}
