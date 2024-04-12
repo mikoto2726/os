aa8492bd094e20faa6f4eacf43cae552d7050c7c
1f10220137 <s1f102201379@iniad.org>
2024-04-12 13:33:38 +0900

    5

--- 01/exercise5.c
+++ 01/exercise5.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+#include <string.h> // strlen関数を使用するために必要
 
 int addnum(void){
     int line_number = 1;
     char buffer[1024];
 
     while (1){
-        if (!gets(buffer)){
+        if (!fgets(buffer, sizeof(buffer), stdin)){
             break;
         }
+        size_t len = strlen(buffer);
+        if (len > 0 && buffer[len-1] == '\n'){
+            buffer[len-1] = '\0';
+        }
+        
         printf("%d %s\n", line_number, buffer);
         line_number++;
     }
